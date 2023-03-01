7a78a7eeadde855552254931e903f0ac72f59020
Riddh Bhattar <riddhbhattar@gmail.com>
2023-03-01 17:25:45 -0600

    train, val - sheet main(test) - sheet

--- main.py
+++ main.py
@@ -11,4 +11,13 @@ input_cols = list(test_df.columns)[1:-1]
 target_col = 'RainTomorrow'
 
 test_inputs = test_df[input_cols].copy()
+test_inputs.drop(columns='Date', inplace=True)
 test_targets = test_df[target_col].copy()
+
+# Numeric & Categorical Cols
+numeric_cols = test_inputs.select_dtypes(include=np.number).columns.tolist()
+categorical_cols = test_inputs.select_dtypes('object').columns.tolist()
+
+print(test_inputs[numeric_cols].describe())
+print(test_inputs[categorical_cols].nunique())
+
--- train_val.py
+++ train_val.py
@@ -12,9 +12,20 @@ input_cols = list(train_df.columns)[1:-1]
 target_col = 'RainTomorrow'
 
 train_inputs = train_df[input_cols].copy()
+train_inputs.drop(columns='Date', inplace=True)
 train_targets = train_df[target_col].copy()
 
 val_inputs = val_df[input_cols].copy()
+val_inputs.drop(columns='Date', inplace=True)
 val_targets = val_df[target_col].copy()
 
-# Train df
+# Numeric & Categorical Cols
+numeric_cols = train_inputs.select_dtypes(include=np.number).columns.tolist()
+categorical_cols = train_inputs.select_dtypes('object').columns.tolist()
+
+print(train_inputs[numeric_cols].describe())
+print(train_inputs[categorical_cols].nunique())
+
+#####
+
+# Imputing Missing Numeric Data
