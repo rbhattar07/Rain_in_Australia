5505b8dde467dbb385dc067c2464bc1caae1dc47
Riddh Bhattar <riddhbhattar@gmail.com>
2023-03-01 14:35:05 -0600

    Initial Commit

---
+++ Data_Exp_Analysis.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+raw_df = pd.read_csv('weatherAUS.csv')
+print(raw_df)
--- exploration&analysis.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import bokeh as bk
-
-raw_df = pd.read_csv('./Rain_in_Australia/weatherAUS.csv')
